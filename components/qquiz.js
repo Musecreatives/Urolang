821aab1a90213a36f2e2ea9782a2aa37fdfc2bc1
gaiusrichardo <gaiyusrichardo@gmail.com>
2022-09-17 23:08:58 +0100

    urolang

---
+++ components/qquiz.js
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faXmark, faArrowLeft, faL} from '@fortawesome/free-solid-svg-icons'
+import styles from '../styles/quick.quiz.module.css'
+
+const RenderQuiz = ({quiz}) => {
+    return(
+        <div className={`${styles['question__wrapper']}`}>
+            {
+                    // console.log(quiz)
+                    
+            }
+        </div>
+
+    )
+}
+
+export default RenderQuiz
--- pages/quick-quiz.js
+++ pages/quick-quiz.js
@@ -4,61 +4,30 @@ import {faXmark, faArrowLeft, faL} from '@fortawesome/free-solid-svg-icons'
 import Loading from '../components/loading'
 import styles from '../styles/quick.quiz.module.css'
 
-const QuickQuiz = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://my-json-server.typicode.com/typicode/demo/posts')
+    const data = await res.json()
+
+    return {
+        props: {quizData: data}
+    }
+
+}
+
+const QuickQuiz = ({quizData}) => {
     const manualLoadTime = 10;
     const [loading, setLoading] = useState(false)
+    const [quiz, setQuiz] = useState(null)
 
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 4000)
-    }, [])
     
     return(
         <div className={styles.quick__quiz__container}>
             {
-                loading?
-                <Loading>
-                    {manualLoadTime}
-                </Loading>
-
-                :
-
-                <div className={`${styles['question__wrapper']}`}>
-                    <div className={styles.quest__cont}>
-                        <div className={styles.heading}>
-                            <div></div>
-                            <h3 className={styles.question}>Choose hello more ques language</h3>
-                            <FontAwesomeIcon icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer", textAlign: 'right'}}/>
-                        </div>
-
-                        <div className={styles.content}>
-
-                            <div className={styles.select__option__container}>
-                                <div className={styles.option__card}>
-                                    <div id="hausa__lang" className={`${styles['option__rep__img']}`}></div>
-                                    <p className={styles.option}>Spoon</p>
-                                </div>
-
-                                <div className={styles.option__card}>
-                                    <div id="igbo__lang" className={`${styles['option__rep__img']}`}></div>
-                                    <p className={styles.option}>Pot</p>
-                                </div>
-
-                                <div className={styles.option__card}>
-                                    <div id="yoruba__lang" className={`${styles['option__rep__img']}`}></div>
-                                    <p className={styles.option}>Bag</p>
-                                </div>
-                            </div>
-
-                            <div className={styles.dlb__btn}>
-                                <button className={`${styles['skip__btn']} ${styles['submit__selected__btn']}`}>Skip</button>
-                                <button className={`${styles['submit__btn']} ${styles['submit__selected__btn']}`}>Proceed</button>
-                            </div>
-                        </div>
+                quizData.map(quizQuest => {
+                    <div key={quizQuest.id}>
+                        {console.log(quizQuest)}
                     </div>
-                </div>
+                })
             }
         </div>
     )
