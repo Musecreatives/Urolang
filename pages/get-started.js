fcda21251f4072ffa214f1a948e5d79920a07bd7
gaiusrichardo <gaiyusrichardo@gmail.com>
2022-09-17 04:26:11 +0100

    quick quiz page

--- pages/get-started.js
+++ pages/get-started.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faXmark, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import {faXmark, faArrowLeft, faL} from '@fortawesome/free-solid-svg-icons'
 import styles from '../styles/get.started.module.css'
 
 const GetStarted = () => {
@@ -75,6 +77,48 @@ const GetStarted = () => {
         document.getElementById('learning__duration').style.display = 'none'
         document.getElementById('difficulty__level').style.display = 'block'
     }
+
+    const router = useRouter()
+    const returnToHome = () => {
+            router.push('/')                
+    }
+    const socialMediumCancelBtn = () =>  returnToHome()
+    const lDurationCancelBtn = () => returnToHome()
+    const levelCancelBtn = () => returnToHome()
+
+    // select difficulty
+    let difficultyLevel;
+    const submitDifficultyLevelBtn = () => {
+        document.getElementById('difficulty__level').style.display = 'none'
+        document.getElementById("select__language").style.display = 'block'
+        
+    }
+
+    // select language
+    let selectedLanguage;
+    const selectedLanguageStyle = (lang1, lang2, lang3) => {
+        document.getElementById("hausa__lang").style.border = lang1
+        document.getElementById("igbo__lang").style.border = lang2
+        document.getElementById("yoruba__lang").style.border = lang3
+    }
+
+    const selectHausa = () => {
+        selectedLanguageStyle('3px solid #7D00C6', 'none', 'none')
+
+        selectedLanguage = 'HAUSA'
+    }
+
+    const selectIgbo = () => {
+        selectedLanguageStyle('none', '3px solid #7D00C6', 'none')
+
+        selectedLanguage = 'IGBO'
+    }
+
+    const selectYoruba = () => {
+        selectedLanguageStyle('none', 'none', '3px solid #7D00C6')
+
+        selectedLanguage = 'EDE YORUBA'
+    }
     
     return(
         <div className={styles.get__started}>
@@ -82,7 +126,7 @@ const GetStarted = () => {
                 <div className={styles.heading}>
                     <h2>How did you hear about Duolingo?</h2>
 
-                    <FontAwesomeIcon icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
+                    <FontAwesomeIcon onClick={socialMediumCancelBtn} icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
                 </div>
 
                 <div className={styles.mediums}>
@@ -123,7 +167,7 @@ const GetStarted = () => {
             <div id="learning__duration" className={`${styles['quest__container']}`} style={{display: 'none'}}>
                 <div className={styles.heading__2}>
                     <FontAwesomeIcon icon={faArrowLeft} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 16px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
-                    <FontAwesomeIcon icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
+                    <FontAwesomeIcon onClick={lDurationCancelBtn} icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
                 </div>
 
                 <div className={styles.content__container}>
@@ -173,7 +217,7 @@ const GetStarted = () => {
                         </div>
                     </div>
 
-                    <button onClick={selectedDurationBtn} className={`${styles['submit__duration__btn']}`}>Proceed</button>
+                    <button onClick={selectedDurationBtn} className={`${styles['submit__btn']}`}>Proceed</button>
                 </div>
             </div>
 
@@ -181,7 +225,7 @@ const GetStarted = () => {
             <div id="difficulty__level" className={styles.quest__container} style={{display: 'none'}}>
                 <div className={styles.heading__2}>
                     <FontAwesomeIcon icon={faArrowLeft} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 16px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
-                    <FontAwesomeIcon icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
+                    <FontAwesomeIcon onClick={levelCancelBtn} icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
                 </div>
 
                 <div className={styles.content__container}>
@@ -222,9 +266,44 @@ const GetStarted = () => {
                         </div>
                     </div>
 
-                    <button className={`${styles['submit__duration__btn']}`}>Proceed</button>
+                    <button onClick={submitDifficultyLevelBtn} className={`${styles['submit__btn']}`}>Proceed</button>
                 </div>
             </div>
+
+            {/* screen 3 */}
+            <div id="select__language" className={`${styles['quest__container']}, ${styles['lang__q__c']}`} style={{display: 'none'}}>
+                <div className={styles.heading__2}>
+                    <FontAwesomeIcon icon={faArrowLeft} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 16px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
+                    <FontAwesomeIcon onClick={lDurationCancelBtn} icon={faXmark} style={{backgroundColor: "#D9D9D9", color: "#9A9A9A", padding: "15px 20px", fontSize: '1.9rem', borderRadius: "50%", cursor: "pointer"}}/>
+                </div>
+
+                <div className={styles.content__container}>
+                    <h3>Choose language</h3>
+
+                    <div className={styles.select__languages__container}>
+                        <div onClick={selectHausa} className={styles.l__card}>
+                            <div id="hausa__lang" className={`${styles['l__descr__img__container']}`}></div>
+                            <p>Hausa</p>
+                        </div>
+
+                        <div onClick={selectIgbo} className={styles.l__card}>
+                            <div id="igbo__lang" className={`${styles['l__descr__img__container']}`}></div>
+                            <p>Igbo</p>
+                        </div>
+
+                        <div onClick={selectYoruba} className={styles.l__card}>
+                            <div id="yoruba__lang" className={`${styles['l__descr__img__container']}`}></div>
+                            <p>Ede Yoruba</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className={styles.speaker}></div>
+                        <button onClick={selectedDurationBtn} className={`${styles['submit__btn']} ${styles['submit__selected__btn']}`}>Proceed</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
